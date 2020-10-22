ec245352f01f3eca74598fe6c7c4bc1e98d8d9f0
Mikołaj Krajewski <mikolajkrajewski.gra@gmail.com>
2020-10-22 20:26:41 +0200

    Imported eviroment assets, changed player movement mechanism to navmeshagent-based

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeedMultiplier;
-    [SerializeField] float moveSpeed;
+    //[SerializeField] float moveSpeed;
 
     void Update()
     {
@@ -14,17 +15,6 @@ public class PlayerController : MonoBehaviour
         float xPosInput = CrossPlatformInputManager.GetAxis("Horizontal");
         float zPosInput = CrossPlatformInputManager.GetAxis("Vertical");
 
-        //Calculating offset
-        float xOffset = xPosInput * moveSpeedMultiplier * Time.deltaTime;
-        float zOffset = zPosInput * moveSpeedMultiplier * Time.deltaTime;
-
-        moveSpeed = Mathf.Max(Mathf.Abs(xOffset * moveSpeedMultiplier), Mathf.Abs(zOffset * moveSpeedMultiplier));
-
-        //Calculating location we end up
-        float positionX = transform.localPosition.x + xOffset;
-        float positionZ = transform.localPosition.z + zOffset;
-
-        //Moving
-        transform.localPosition = new Vector3 (positionX,transform.localPosition.y,positionZ);
+        GetComponent<NavMeshAgent>().destination = new Vector3(transform.position.x + xPosInput, transform.position.y, transform.position.z + zPosInput);
     }    
 }
