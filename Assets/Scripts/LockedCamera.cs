7c9a81db26df79deffd46081442d53f58a60b37b
Mikołaj Krajewski <mikolajkrajewski.gra@gmail.com>
2020-10-22 18:21:02 +0200

    Implemented a camera system that locks and follows player's z - position

---
+++ Assets/Scripts/LockedCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockedCamera : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float cameraSpeed;
+
+    void Awake()
+    {
+        transform.position = target.position;
+    }
+    
+    void Update()
+    {
+        float distance = cameraSpeed * Time.deltaTime;
+        Vector3 target_position_z = new Vector3(transform.position.x, transform.position.y, target.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, target_position_z, distance);
+    }
+}
