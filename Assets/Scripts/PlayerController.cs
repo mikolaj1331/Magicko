5800044c2d5b52636f06a80bc1526702f7e8378d
Mikołaj Krajewski <mikolajkrajewski.gra@gmail.com>
2020-10-23 14:49:57 +0200

    Added in animator for player and set up animation handling for running

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,10 +11,27 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        MovePlayer();
+        UpdateAnimator();
+    }
+
+    private void MovePlayer()
+    {
         // Getting input values
         float xPosInput = CrossPlatformInputManager.GetAxis("Horizontal");
         float zPosInput = CrossPlatformInputManager.GetAxis("Vertical");
-
+        //Moving the character
         GetComponent<NavMeshAgent>().destination = new Vector3(transform.position.x + xPosInput, transform.position.y, transform.position.z + zPosInput);
-    }    
+    }
+
+    void UpdateAnimator()
+    {
+        //Getting velocity values
+        Vector3 playerVelocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localPlayerVelocity = transform.InverseTransformDirection(playerVelocity);
+
+        //Setting a movement speed variable for animator tree blend
+        float moveSpeed = localPlayerVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed",moveSpeed);
+    }
 }
