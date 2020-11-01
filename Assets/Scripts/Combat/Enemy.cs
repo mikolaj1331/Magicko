95b90de8aed53ba8b9735496ba5f7427a8a49b93
Mikołaj Krajewski <mikolajkrajewski.gra@gmail.com>
2020-11-01 16:27:00 +0100

    Implemented the base for enemy combat behaviour

--- Assets/Scripts/Combat/Enemy.cs
+++ Assets/Scripts/Combat/Enemy.cs
@@ -1,11 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Magicko.Control;
 using UnityEngine;
 
 namespace Magicko.Combat
 {
     public class Enemy : MonoBehaviour
     {
+        [SerializeField] Transform target;
+        [SerializeField] float detectRange = 10;
         
+        private void Start() 
+        {
+            target = FindObjectOfType<PlayerController>().transform;
+        }
+
+        private void Update() 
+        {
+            if(Vector3.Distance(transform.position, target.position) <= detectRange)
+            {
+                GetComponent<CombatHandler>().Attack(target);
+            }    
+        }
     }
 }
