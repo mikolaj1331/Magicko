ad9754172e3a5d709c91a763cafc1c2f1e7e47df
Mikołaj Krajewski <mikolajkrajewski.gra@gmail.com>
2020-11-21 21:42:29 +0100

    Fixed the rotation bug, fixed the projectile behaviour, added animations for further development

--- Assets/Scripts/Combat/ProjectileHandler.cs
+++ Assets/Scripts/Combat/ProjectileHandler.cs
@@ -9,15 +9,11 @@ namespace Magicko.Combat
     public class ProjectileHandler : MonoBehaviour
     {
         [SerializeField] ParticleSystem particleSystemFx;
-        [SerializeField] Transform parent;
         [SerializeField] float projectileSpeed = 0.01f;
 
         private void Start() 
         {
-            Quaternion quat = parent.localRotation;
-            quat.eulerAngles -= new Vector3(0,180,0);
-            //parent.localRotation
-            Instantiate(particleSystemFx,parent.position,quat,parent);  
+ 
             particleSystemFx.Play();
         }
         private void Update() 
@@ -27,7 +23,6 @@ namespace Magicko.Combat
 
         private void MoveTowards()
         {
-            //transform.position = Vector3.MoveTowards(transform.position,destination,projectileSpeed) * Time.deltaTime;
             transform.localPosition += transform.forward * projectileSpeed * Time.deltaTime;
         }
 
@@ -43,6 +38,3 @@ namespace Magicko.Combat
         }
     }
 }
-
-// (SOLVED) TODO: ParticleSystem does not rotate correctly
-// (SOLUTION) Correct way was to rotate using euler angles and vector 3 values for rotation and not rotating Quaternion directly
--- Assets/Scripts/Movement/MovementHandler.cs
+++ Assets/Scripts/Movement/MovementHandler.cs
@@ -13,7 +13,7 @@ namespace Magicko.Movement
 
         private void Start() 
         {
-            navigationAgent = GetComponent<NavMeshAgent>();    
+            navigationAgent = GetComponent<NavMeshAgent>();
         }
 
         private void Update() 
@@ -34,9 +34,12 @@ namespace Magicko.Movement
 
         public void MoveTowards(Vector3 destination)
         {
+            navigationAgent.updateRotation = true;
             // Sets the destination of navigation agent as the one passed in the method argument
             navigationAgent.destination = destination;
             navigationAgent.isStopped = false;
+            if (transform.position == destination)
+                navigationAgent.updateRotation = false;
         }
 
         public void MoveAction(Vector3 destination)
@@ -52,3 +55,7 @@ namespace Magicko.Movement
         }
     }
 }
+
+//TODO: NavMeshAgent rotates the player upon arrival at the destination
+//Solution: The navigator couldn't reach the exact value of float variable due to specifics of this variable,
+//increased stopping distance parameter to stop the navigator from looking for perfect position
